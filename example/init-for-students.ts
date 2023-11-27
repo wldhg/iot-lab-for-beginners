13d9ba08b2701d4decde0dc5f4e6ceec632ef8ec
wldhg <jio@wldhg.com>
2023-11-27 20:22:29 +0900

    added init script

---
+++ example/init-for-students.ts
@@ -0,0 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+
+fs.rmSync(path.join(__dirname, '../.git'), { force: true, recursive: true });
+fs.rmSync(path.join(__dirname, '../components/SliderControl.tsx'), { force: true });
+fs.rmSync(path.join(__dirname, '../components/SliderControl.module.scss'), { force: true });
+fs.rmSync(path.join(__dirname, '../example/ArduinoMega-cpp'), { force: true, recursive: true });
+fs.rmSync(path.join(__dirname, '../example/ESP32-cpp'), { force: true, recursive: true });
+fs.rmSync(path.join(__dirname, '../example/inference'), { force: true, recursive: true });
+fs.rmSync(path.join(__dirname, '../pages/panel/example1.tsx'), { force: true });
+fs.rmSync(path.join(__dirname, '../pages/panel/example2.tsx'), { force: true });
+fs.rmSync(path.join(__dirname, '../README.md'), { force: true });
+fs.rmSync(path.join(__dirname, '../public/screenshot-web.png'), { force: true });
+fs.rmSync(path.join(__dirname, '../public/screenshot-wokwi.png'), { force: true });
+fs.rmSync(path.join(__dirname, '../.gitignore'), { force: true });
+fs.rmSync(path.join(__dirname, '../LICENSE'), { force: true });
+fs.rmSync(path.join(__dirname, '../example/init-for-students.ts'), { force: true });
